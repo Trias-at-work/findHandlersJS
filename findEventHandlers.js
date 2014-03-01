e63b6afc9e99871b225009dc38e77d04854355d4
Trias <stefan.ackermann42@gmail.com>
2014-03-01 19:36:03 +0100

    Update findEventHandlers.js
    Changed interface so it can be used for all events or multiple events.
    No Arguments means all events for all elements.
    Changed output generated to a more accessible form.
    Some refactoring.

--- findEventHandlers.js
+++ findEventHandlers.js
@@ -1,6 +1,17 @@
 ﻿var findEventHandlers = function (eventType, jqSelector) {
-    var results = [];
     var $ = jQuery;// to avoid conflict between others frameworks like Mootools
+    var anyEvent = false;
+    var eventTypes = [];
+    
+    if(!jqSelector){
+        jqSelector = '*';
+    }
+    
+    if(!eventType){
+        anyEvent = true;
+    }else{
+        eventTypes = eventType.split(' ');
+    }
 
     var arrayIntersection = function (array1, array2) {
         return $(array1).filter(function (index, element) {
@@ -11,27 +22,20 @@
     var haveCommonElements = function (array1, array2) {
         return arrayIntersection(array1, array2).length !== 0;
     };
+    
+    var eventInfo = {};
 
 
     var addEventHandlerInfo = function (element, event, $elementsCovered) {
+        eventInfo[event.type] = eventInfo[event.type] || {};
+        var selector = element.localName || '#document';
+        var events = eventInfo[event.type][selector] = eventInfo[event.type][selector] || [];
         var extendedEvent = event;
-        if ($elementsCovered !== void 0 && $elementsCovered !== null) {
-            $.extend(extendedEvent, { targets: $elementsCovered.toArray() });
-        }
-        var eventInfo;
-        var eventsInfo = $.grep(results, function (evInfo, index) {
-            return element === evInfo.element;
-        });
-
-        if (eventsInfo.length === 0) {
-            eventInfo = {
-                element: element,
-                events: [extendedEvent]
-            };
-            results.push(eventInfo);
-        } else {
-            eventInfo = eventsInfo[0];
-            eventInfo.events.push(extendedEvent);
+        
+        $.merge(extendedEvent, {targets: $elementsCovered.toArray() });
+        
+        if($.inArray(events, extendedEvent) === -1){
+            events.push(extendedEvent);
         }
     };
 
@@ -43,22 +47,23 @@
 
     $.each($allElements, function (elementIndex, element) {
         var allElementEvents = $._data(element, "events");
-        if (allElementEvents !== void 0 && allElementEvents[eventType] !== void 0) {
-            var eventContainer = allElementEvents[eventType];
-            $.each(eventContainer, function(eventIndex, event){
-                var isDelegateEvent = event.selector !== void 0 && event.selector !== null;
-                var $elementsCovered;
-                if (isDelegateEvent) {
-                    $elementsCovered = $(event.selector, element); //only look at children of the element, since those are the only ones the handler covers
-                } else {
-                    $elementsCovered = $(element); //just itself
-                }
-                if (haveCommonElements($elementsCovered, $elementsToWatch)) {
-                    addEventHandlerInfo(element, event, $elementsCovered);
-                }
+        if (allElementEvents !== void 0) {
+            $.each(allElementEvents, function(i, eventContainer){
+                $.each(eventContainer, function(eventIndex, event){
+                    var isDelegateEvent = event.selector !== void 0 && event.selector !== null;
+                    var $elementsCovered;
+                    if (isDelegateEvent) {
+                        $elementsCovered = $(event.selector, element); //only look at children of the element, since those are the only ones the handler covers
+                    } else {
+                        $elementsCovered = $(element); //just itself
+                    }
+                    if (haveCommonElements($elementsCovered, $elementsToWatch)) {
+                        addEventHandlerInfo(element, event, $elementsCovered);
+                    }
+                });
             });
         }
     });
 
-    return results;
+    return eventInfo;
 };
